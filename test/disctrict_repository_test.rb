29e0622be85e2587d1784acfa14e5a76747a2020
MasonHolland <masontholland@gmail.com>
2017-04-11 11:09:55 -0600

    district and test created

---
+++ test/disctrict_repository_test.rb
@@ -0,0 +1,13 @@
+require './test/helper'
+require_relative "../../headcount/lib/district_repository"
+require_relative "../../headcount/lib/district"
+
+class DistrictRepositoryTest < Minitest::Test
+
+  def test_district_repo_exists
+    dr = DistrictRepository.new
+    assert_instance_of DistrictRepository, dr
+  end
+
+  
+end
