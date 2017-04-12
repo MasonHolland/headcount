9f8556b86b8c3d3622f08931f874e503cc30135b
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-12 15:05:54 -0600

    Commits change to enrollment test

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -4,7 +4,8 @@ require './lib/enrollment'
 class TestEnrollment < Mintest::Test
 
   def test_it_exists
-    e = Enrollment.new{:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+    
     assert_instance_of Enrollment, e
   end
 end
