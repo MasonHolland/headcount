fc3ccf3b78f26dc7364069f908b28e4ce402ebbf
MasonHolland <masontholland@gmail.com>
2017-04-11 22:06:38 -0600

    got first test passing

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -3,38 +3,27 @@ require_relative 'district'
 require 'pry'
 
 class DistrictRepository
-  attr_accessor :data , :name
-  attr_accessor :districts
-  
+  attr_accessor :contents, :districts, :name
+
   def initialize
-    @districts = []
+    @districts = {}
   end
 
 
-  # def load_data(path)
-    # filename = path[:enrollment][:kindergarten]
-    # CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
-    #   name = row[:location].upcase
-    #   dis = District.new(:name => name.upcase)
-    #   @districts[dis.name] = dis
-      # end
-  def load_data(args)
-    @data = CSV.open args[:enrollment][:kindergarten], headers: true, header_converters: :symbol
-    # data.each do |row|
-    #   name = row[:Location]
-    # end
-    # binding.pry
+  def load_data(path)
+    filename = path[:enrollment][:kindergarten]
+    CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+      name = row[:location].upcase
+      dis = District.new({:name => name.upcase}, self)
+      @districts[dis.name] = dis unless @districts.has_key?(name)
+      end
   end
 
   def find_by_name(name)
-    data.each do |row|
-      if row[:location] == name.upcase
-        districts = District.new(row)
-      end
-      # binding.pry
+    @districts[name.upcase]
     end
-  end
 
+binding.pry
 end
 
 # binding.pry
---
+++ test/disctrict_repository_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require "./lib/district_repository"
+require "./lib/district"
+
+class DistrictRepositoryTest < Minitest::Test
+
+  def test_district_repo_exists
+    dr = DistrictRepository.new
+    assert_instance_of DistrictRepository, dr
+  end
+
+  def test_district_repo_can_find_one_district_by_name
+    dr = DistrictRepository.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    district = dr.find_by_name("Adams County 14")
+    assert_equal "ADAMS COUNTY 14", district.name
+    # assert_instance_of District, district
+  end
+
+end
