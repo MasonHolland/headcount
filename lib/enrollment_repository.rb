5f98efd5620360566f08c7d7ca6bbf7d0adeccf7
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-12 14:41:54 -0600

    Commits enrollment repo and enrollment files with passing tests

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -2,7 +2,7 @@ require 'csv'
 require_relative 'enrollment'
 require 'pry'
 
-class EnrollmentDepository
+class EnrollmentRepository
   def initialize
     @enrollments = {}
   end
@@ -10,8 +10,8 @@ class EnrollmentDepository
   def load_data(path)
     CSV.foreach(path[:enrollment][:kindergarten], headers: true, header_converters: :symbol) do |row|
       name = row[:location].upcase
-      enrollments = Enrollment.new({:name => name}, self)
-      @enrollments[district.name] = enrollment unless @enrollments.has_key?(name)
+      enrollment = Enrollment.new({:name => name}, self)
+      @enrollments[enrollment.name] = enrollment unless @enrollments.has_key?(name)
     end
   end
 
