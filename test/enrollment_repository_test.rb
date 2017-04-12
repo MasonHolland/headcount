41cdae9cffc4510915d3ea32e4481c47887965f2
MasonHolland <masontholland@gmail.com>
2017-04-12 14:10:00 -0600

    got first test for enrollment repo sorted

---
+++ test/enrollment_repository_test.rb
@@ -0,0 +1,16 @@
+require './test/test_helper'
+
+class TestEnrollmentRepository < Minitest::Test
+
+  def test_enrollment_repo_exists
+    er = EnrollmentRepository.new
+    assert_instance_of EnrollmentRepository, er
+  end
+
+  def test_can_find_by_name
+    er = EnrollmentRepository.new
+    er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    enrollment = er.find_by_name("ACADEMY 20")
+    assert_equal "ACADEMY 20", enrollemnt.name
+  end
+end
