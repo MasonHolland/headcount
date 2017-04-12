0ca76686a2e119c2d2ab8b9f3c60be6aacfce7d3
MasonHolland <masontholland@gmail.com>
2017-04-12 11:37:25 -0600

    first two tests passing. District repo and district built

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -9,22 +9,17 @@ class DistrictRepository
     @districts = {}
   end
 
-
   def load_data(path)
-    filename = path[:enrollment][:kindergarten]
-    CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+    CSV.foreach(path[:enrollment][:kindergarten], headers: true, header_converters: :symbol) do |row|
       name = row[:location].upcase
-      dis = District.new({:name => name.upcase}, self)
-      @districts[dis.name] = dis unless @districts.has_key?(name)
-      end
+      district = District.new({:name => name}, self)
+      @districts[district.name] = district unless @districts.has_key?(name)
+    end
   end
 
   def find_by_name(name)
     @districts[name.upcase]
-    end
+  end
 
-binding.pry
+
 end
-
-# binding.pry
-""
