6b564792879e368e73a6f8f03a529b74683af430
MasonHolland <masontholland@gmail.com>
2017-04-12 14:20:17 -0600

    enrollment test built. improved formatting

--- test/district_test.rb
+++ test/district_test.rb
@@ -2,12 +2,9 @@ require_relative '../lib/district'
 require './test/test_helper'
 
 class TestDistrict < Minitest::Test
+
   def test_the_object_is_a_district
     d = District.new({:name => "ACADEMY 20"})
     assert_instance_of District, d
   end
-  #
-  # def test
-  # end
-
 end
--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -1,4 +1,5 @@
 require './test/test_helper'
+require './lib/enrollment_repository'
 
 class TestEnrollmentRepository < Minitest::Test
 
