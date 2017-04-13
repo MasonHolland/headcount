234d63a02068542922df73247364dd1ea5ad911e
MasonHolland <masontholland@gmail.com>
2017-04-13 16:53:35 -0600

    still working on getting rake to pass on iteration zero

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,5 +1,3 @@
-
-require 'bigdecimal'
 require 'pry'
 class Enrollment
   attr_reader :name, :kindergarten_participation
@@ -7,7 +5,7 @@ class Enrollment
   def initialize(input)
     @name = input[:name]
     @kindergarten_participation = input[:kindergarten_participation]
-    
+
   end
 
   def kindergarten_participation_by_year
@@ -18,6 +16,6 @@ class Enrollment
   end
 
   def kindergarten_participation_in_year(year)
-    @kindergarten_participation[year].to_s[0..4].to_f
+    kindergarten_participation[year].to_s[0..4].to_f
   end
 end
--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -12,21 +12,17 @@ class EnrollmentRepository
     CSV.foreach(path[:enrollment][:kindergarten], headers: true, header_converters: :symbol) do |row|
       name = row[:location].upcase
       years = row[:timeframe]
-      enrollment = Enrollment.new({:name => name})
-      @enrollments[enrollment.name] = enrollment unless @enrollments.has_key?(name)
+      enrollment = Enrollment.new({:name => name, :kindergarten_participation => :years})
+      @enrollments << enrollment if @enrollments.none? { |existing| existing.name == enrollment.name }
     end
   end
 
   def find_by_name(name)
-    @enrollments[name.upcase]
+    @enrollments.find {|enrlmnt| enrlmnt.name == name.upcase}
   end
-
-  # def kindy_part_storage
-  #   @enrollments[name].kindergarten_participation
+  #
+  # def kindergarten_participation
+  #   @enrollments[years]
   # end
-
-  def kindergarten_participation
-    @enrollments[years]
-  end
 
 end
