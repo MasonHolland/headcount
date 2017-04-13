53a33dc17c15ea648873b3221e4c9f37801c1e0a
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-13 13:58:55 -0600

    Commits change from districts initializing as a hash to array

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -14,11 +14,23 @@ class DistrictRepository
       name = row[:location].upcase
       district = District.new({:name => name}, self)
       @districts[district.name] = district unless @districts.has_key?(name)
+      # binding.pry
     end
   end
 
   def find_by_name(name)
     @districts[name.upcase]
+  end
+
+  def find_all_matching(name)
+    inc = 0
+    if districts == {}
+      return []
+    else
+      [districts.include?(districts[name.upcase])]
+      inc += 1
+    end
+    inc
   end
 
 
--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -12,7 +12,7 @@ class TestEnrollment < Minitest::Test
     er = EnrollmentRepository.new
     er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
     e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
-    assert_equal e.kindergarten_participation_by_year, {2010=>0.391, 2011=>0.353, 2012=>0.267}
+    assert_equal e.kindergarten_participation_by_year, {2010=>0.391, 2011=>0.354, 2012=>0.268}
   end
 
   def test_kindergarten_participation_in_year
