c2e567b3a999b2cc0ae4fa0b27c7b21a29293aff
MasonHolland <masontholland@gmail.com>
2017-04-13 11:49:33 -0600

    all tests passing, iteration 0 complete

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -11,22 +11,13 @@ class Enrollment
   end
 
   def kindergarten_participation_by_year
-    # binding.pry
-    # kindergarten_participation.each do |k, v|
-      # kindergarten_participation[k] = v
       kp = kindergarten_participation
       kp.update(kp) do |key, v|
         v.to_s[0..4].to_f
-        # v[0..4]
-        # v.to_f
-            # value = value.to_s
-      # value = value[0..4].to_f
-    # end|key, value| valufloor(3)}
-      # value.round(3)
-    end
+      end
   end
 
   def kindergarten_participation_in_year(year)
-    @kindergarten_participation[year]
+    @kindergarten_participation[year].to_s[0..4].to_f
   end
 end
