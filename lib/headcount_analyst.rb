05db6b3454d4095c395cbf4dd4bd4a81b032a06e
MasonHolland <masontholland@gmail.com>
2017-04-17 16:49:15 -0600

    passing iteration 1"

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -19,7 +19,10 @@ class HeadcountAnalyst
     end
 
     def kindergarten_participation_rate_variation_trend(region_a, region_b)
-      
-
+      find_district = @district_repository.find_by_name(region_a)
+      find_against = @district_repository.find_by_name(region_b[:against])
+      a = find_district.enrollment.kindergarten_participation_by_year.sort.to_h
+      b = find_against.enrollment.kindergarten_participation_by_year.sort.to_h
+      c = a.merge(b){|key, oldval, newval| (oldval / newval).to_s[0..4].to_f}
     end
 end
