48072b9da7e9c60aef48e33e84c33236733738e7
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-17 14:03:34 -0600

    Commits transition over to hash with passing rake (iteration zero)

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -11,9 +11,7 @@ class HeadcountAnalyst
     end
 
     def kindergarten_participation_rate_variation(region_a, region_b)
-      binding.pry
-
-
+      # binding.pry
       baseline = @district_repository.find_by_name(region_a).enrollment.kindergarten_participation_by_year.values.reduce(:+)/11
       against = @district_repository.find_by_name(region_b[:region_b]).enrollment.kindergarten_participation_by_year.reduce(:+)/11
       baseline / against
