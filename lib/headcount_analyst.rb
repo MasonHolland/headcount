626103891bef176f84329754adf4a40addd94f03
MasonHolland <masontholland@gmail.com>
2017-04-17 16:15:19 -0600

    passing headcount tests. completing iteration 1

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -13,15 +13,8 @@ class HeadcountAnalyst
     end
 
     def kindergarten_participation_rate_variation(region_a, region_b)
-      baseline = @district_repository.find_by_name(region_a)
-      against = @district_repository.find_by_name(region_b[:against])
-      binding.pry
-
-
-      baslne = baseline.enrollment.kindergarten_participation_by_year
-      agnst = against.enrollment.kindergarten_participation_by_year
-      b = (baslne.values.reduce(:+)/baslne.values.count)
-      a = (agnst.values.reduce(:+)/agnst.values.count)
-      b / a
+      baseline = @district_repository.find_by_name(region_a).enrollment.kindergarten_participation_by_year.values.reduce(:+)/11
+      against = @district_repository.find_by_name(region_b[:against]).enrollment.kindergarten_participation_by_year.values.reduce(:+)/11
+      (baseline / against).to_s[0..4].to_f
     end
 end
