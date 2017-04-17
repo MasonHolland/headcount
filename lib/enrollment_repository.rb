f407d1e9857f0baa3d049d74e1f5a209b154061c
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-17 10:44:46 -0600

    Commits update to find_by_name method in enrollment repo

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -9,21 +9,22 @@ class EnrollmentRepository
   end
 
   def load_data(path)
-    path[:enrollment].each do |symbol, filepath|
-    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-      name = row[:location]
-      year = row[:timeframe].to_i
-      if @enrollments[name] 
-        @enrollments[name].kindergarten_participation[year] = row[:data]
-      else
-      enro = Enrollment.new({:name => name, :kindergarten_participation => {year => row[:data]}})
-      @enrollments[name] = enro
+    path[:enrollment].each do |symbol, file_path|
+      CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+        name = row[:location]
+        year = row[:timeframe].to_i
+        if @enrollments[name] 
+          @enrollments[name].kindergarten_participation[year] = row[:data]
+        else
+          enro = Enrollment.new({:name => name, :kindergarten_participation => {year => row[:data]}})
+          @enrollments[name] = enro
+        end
       end
     end
   end
 
   def find_by_name(name)
-    @enrollments.find {|enrlmnt| enrlmnt.name == name.upcase}
+    @enrollments[name.upcase] if name
   end
 
   def enrollment_data_framework(name, year, row)
