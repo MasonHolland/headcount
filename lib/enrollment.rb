cfba210c3f87869462a2661079807f17b555449f
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-18 06:11:30 -0600

    Comits high_school_graduation as attr_accessor to allow to populate from CSV

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class Enrollment
-  attr_accessor :name, :kindergarten_participation
+  attr_accessor :name, :kindergarten_participation, :high_school_graduation
 
     def initialize(input)
       @name = input[:name]
       @kindergarten_participation = input[:kindergarten_participation]
-      @high_school_graducation = input[:high_school_graduation]
+      @high_school_graduation = input[:high_school_graduation]
     end
 
     def kindergarten_participation_by_year
