3f697a0a3cd4f22e9546cd50ac2ffa8471a4d2e5
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-18 07:11:30 -0600

    Commits graduation rate by year for enrollment and enrollment repo tests

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -68,9 +68,11 @@ class TestEnrollmentRepository < Minitest::Test
     er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv",
      :high_school_graduation => "./data/High school graduation rates.csv"}})
 
-    enrollment = er.graduation_rate_by_year("ACADEMY 20")
-
+    enrollment = er.find_by_name("ACADEMY 20")
+    actual = enrollment.graduation_rate_by_year
     expect = { 2010 => 0.895, 2011 => 0.895, 2012 => 0.889, 2013 => 0.913, 2014 => 0.898}
+  
+    assert_equal expect, actual
   end
 
   def test_graduation_rate_in_year
