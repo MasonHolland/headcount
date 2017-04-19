bd2941923cfd978449d4e001f73921124ee73011
MasonHolland <masontholland@gmail.com>
2017-04-19 15:37:51 -0600

    creating error class

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -15,4 +15,34 @@ class StateWideTest
       input.has_key?(:race_year_subject)
     end
 
+    def proficient_by_grade(grade)
+      cleaned_grade = grade.to_s[0..4].to_f
+      raise UnknownDataError unless @grade_year_subject[cleaned_grade]
+        race_year_subject[cleaned_grade]
+    end
+
+    def proficient_by_race_or_ethnicity(race)
+      raise UnknownDataError unless @race_year_subject[race]
+        @race_year_subject[race]
+    end
+
+    def proficient_for_subject_by_grade_in_year(subject, grade, year)
+      raise UnknownDataError unless check_grade_keys(grade, year, subject)
+        @grade_year_subject[grade.to_s[0..4].to_f][year][subject]
+    end
+
+    def proficient_for_subject_by_race_in_year(subject, race, year)
+      raise UnknownDataError unless check_race_keys(race, year, subject)
+        @race_year_subject[race][year][subject]
+    end
+
+    def check_grade_keys(grade, year, subject)
+      gys = @grade_year_subject
+      (gys[grade.to_s[0..4].to_f] && gys[grade.to_s[0..4].to_f][year] && gys[grade.to_s[0..4].to_f][year][subject])
+    end
+
+    def check_race_keys(race, year, subject)
+      rys = @race_year_subject
+      (rys[race] && rys[race][year] && rys[race][year][subject])
+    end
   end
--- test/statewide_test_test.rb
+++ test/statewide_test_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 require_relative '../lib/statewide_test'
 require_relative '../lib/statewide_test_repository'
 
-class StatewideTestTest < Minitest::Test
+class StateWideTestTest < Minitest::Test
 
   def data
     {name: "COLORADO",
@@ -12,53 +12,53 @@ class StatewideTestTest < Minitest::Test
   end
 
   def test_it_exists
-    st = StatewideTest.new
-    assert_instance_of StatewideTest, st
+    st = StateWideTest.new
+    assert_instance_of StateWideTest, st
   end
 
   def test_statewide_test_has_name
-    st = StatewideTest.new(data)
+    st = StateWideTest.new(data)
     assert_equal "COLORADO", st.name
     assert_equal String, st.name.class
   end
 
   def test_statewide_test_has_grade_data
-    st = StatewideTest.new(data)
+    st = StateWideTest.new(data)
     actual = ({:third_grade=>{2008=>{:math=>0.697}}})
     assert_equal actual, st.grade_year_subject
     assert_equal Hash, st.grade_year_subject.class
   end
 
   def test_statewide_test_has_race_data
-   st = StatewideTest.new(data)
+   st = StateWideTest.new(data)
    actual = {:all_students=>{2011=>{:math=>0.557}}}
    assert_equal actual, st.race_year_subject
    assert_equal Hash, st.race_year_subject.class
  end
 
  def test_proficient_by_grade
-   st = StatewideTest.new(data)
+   st = StateWideTest.new(data)
    actual = {2008=>{:math=>0.697}}
    assert_equal actual, st.proficient_by_grade(:third_grade)
    assert_equal Hash, st.proficient_by_grade(:third_grade).class
  end
 
  def test_proficient_by_race
-   st = StatewideTest.new(data)
+   st = StateWideTest.new(data)
    actual = {2011=>{:math=>0.557}}
    assert_equal actual, st.proficient_by_race_or_ethnicity(:all_students)
    assert_equal Hash, st.proficient_by_race_or_ethnicity(:all_students).class
  end
 
  def test_proficient_for_subject_by_grade_in_year
-   st = StatewideTest.new(data)
+   st = StateWideTest.new(data)
    expected = st.proficient_for_subject_by_grade_in_year(:math, :third_grade, 2008)
    assert_equal 0.697, expected
    assert_equal Float, expected.class
  end
 
  def test_proficient_for_subject_by_race_in_year
-   st = StatewideTest.new(data)
+   st = StateWideTest.new(data)
    expected = st.proficient_for_subject_by_race_in_year(:math, :all_students, 2011)
    assert_equal 0.557, expected
    assert_equal Float, expected.class
