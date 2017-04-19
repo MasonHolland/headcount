4ed6beddcaa6d651f554413a782b7366ddba39f9
MasonHolland <masontholland@gmail.com>
2017-04-19 11:09:30 -0600

    more tests

--- test/statewide_test_test.rb
+++ test/statewide_test_test.rb
@@ -15,3 +15,20 @@ require_relative '../lib/statewide_test_repository'
       st = StatewideTest.new
       assert_instance_of StatewideTest, st
     end
+
+    def test_statewide_test_has_name
+      st = StatewideTest.new(data)
+      assert_equal "COLORADO", st.name
+      assert_equal String, st.name.class
+    end
+
+    def test_statewide_test_has_grade_data
+      st = StatewideTest.new(data)
+      actual = ({:third_grade=>{2008=>{:math=>0.697}}})
+      assert_equal actual, st.grade_year_subject
+      assert_equal Hash, st.grade_year_subject.class
+    end
+
+
+
+end
