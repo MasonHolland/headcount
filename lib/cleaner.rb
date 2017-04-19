1f6f9750417229c117dc0acaae5f9d6c246538f6
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-19 05:54:21 -0600

    Cleaner method and tests

---
+++ lib/cleaner.rb
@@ -0,0 +1,62 @@
+
+class Cleaner
+
+  def self.truncate(float)
+    float = 0 if float.nan?
+    (float * 1000).floor / 1000.to_f
+  end
+
+  def self.input_name(row)
+    row[:location].upcase
+  end
+
+  def self.poverty_level(row)
+    row[:poverty_level] unless row[:poverty_level].nil?
+  end 
+  
+  def self.dataformat(row)
+    row[:dataformat].downcase.to_sym
+  end
+
+  def self.year(row, symbol = nil)
+    if symbol == :median_household_income
+      x = row[:timeframe].split("-")
+      results = x.map {|year| year.to_i}
+    else
+      row[:timeframe].to_i
+    end
+  end
+
+  def self.data(row)
+    data = row[:data]
+    if data.nil?
+      0
+    elsif data != nil && data.include?(".")
+      data.to_f.round(3)
+    elsif data != Fixnum ||  data != float
+      "N/A"
+    else
+      data.to_i
+    end
+  end
+
+  def self.pov_year(row)
+    if row[:dataformat] == "percent"
+      row[:timeframe].to_i
+    else
+      nil
+    end
+  end
+
+  def self.lunch_data(row)
+    return row[:data].to_f.round(3) if row[:dataformat].downcase == "percent"
+    return row[:data].to_i if row[:dataformat].downcase == "number"
+  end
+
+  def self.grade(grade)
+    return :third_grade  if grade == 3
+    return :eighth_grade if grade == 8
+    return grade if grade == :third_grade || grade == :eighth_grade
+  end
+
+end
--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -1,8 +1,23 @@
 require "csv"
 
 class EconomicProfileRepository
+  
   def initialize
     @economic_profiles = {}
     @row_object = ""
   end
+
+  def load_data(path)
+    path[:economic_profile].each do |symbol, filepath|
+      CSV.foreach(filepath, headers: true, header_converters: symbol) do |row|
+        @row_object = row
+        populate_eco_repo(row, symbol)
+      end
+    end
+  end
+
+  def find_by_name(name)
+    @economic_profiles[name]
+  end
+
 end
