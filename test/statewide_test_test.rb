da2d8e1b6811158870aef2cebc9a6dac5409d3f9
MasonHolland <masontholland@gmail.com>
2017-04-19 11:05:27 -0600

    written first test for statewide test

---
+++ test/statewide_test_test.rb
@@ -0,0 +1,12 @@
+require_relative 'test_helper'
+require_relative '../lib/statewide_test'
+require_relative '../lib/statewide_test_repository'
+
+  class StatewideTestTest < Minitest::Test
+
+    def data
+      {name: "COLORADO",
+       grade_year_subject: {:third_grade=>{2008=>{:math=>0.697}}},
+       race_year_subject: {:all_students=>{2011=>{:math=>0.557}}},
+       asian: {2011=>{:math=>0.709}}}
+    end
