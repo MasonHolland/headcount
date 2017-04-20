ca1ea9c2932425add6e87c9a2498c7730adfb599
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-19 18:05:16 -0600

    Commits eco prof repo tests all passing

--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -9,9 +9,7 @@ class TestEconomicProfileRepository < Minitest::Test
   end
 
   def test_repo_can_populate_economic_profiles
-    # skip
     er = EconomicProfileRepository.new 
-    binding.pry
     er.load_data({:economic_profile => {
       :median_household_income => "./data/Median household income.csv",
       :children_in_poverty => "./data/School-aged children in poverty.csv",
@@ -24,7 +22,6 @@ class TestEconomicProfileRepository < Minitest::Test
   end
 
   def test_repo_can_find_by_name
-    # skip
     er = EconomicProfileRepository.new 
     er.load_data({:economic_profile => {
       :median_household_income => "./data/Median household income.csv",
@@ -36,5 +33,22 @@ class TestEconomicProfileRepository < Minitest::Test
     eco_profile = er.find_by_name("ACADEMY 20")
     assert_equal "ACADEMY 20", eco_profile.name    
   end
+
+  def test_enrollments_stored_in_hash
+    er = EconomicProfileRepository.new
+    er.load_data({:economic_profile => {
+      :median_household_income => "./data/Median household income.csv",
+      :children_in_poverty => "./data/School-aged children in poverty.csv",
+      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+      :title_i => "./data/Title I students.csv"
+      }})
+    assert_equal Hash, er.economic_profiles.class
+    assert_equal 181, er.economic_profiles.count
+  end
   
+  def test_ep_data_framework
+    er = EconomicProfileRepository.new
+    assert_equal Hash, er.ep_data_framework("Academy 20").class
+  end
+
 end
